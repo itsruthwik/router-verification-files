fa1501d70b7b18a2e30cc35b45477c7bdca3bfb7
Ruthwik <itsruthwik@outlook.com>
2024-07-07 18:05:22 -0700

    Add new taskrun files

---
+++ new/SRC/sub_module/local_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Local Decoders for Multiplexers
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Sun Jul  7 02:19:44 2024
+//-------------------------------------------
