745ab2efcf9038082218dc653b25a68803fb8943
Ruthwik <itsruthwik@outlook.com>
2024-07-29 20:50:17 -0700

    Add openfpga run files with dummy router2

---
+++ nocgen_router/openfpga-runs/dummy-run01/SRC/sub_module/shift_register_banks.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Shift register banks used in FPGA
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Mon Jul 29 20:37:44 2024
+//-------------------------------------------
